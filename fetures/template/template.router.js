298289cf4ed2054f49f028f563a2cd06948abeed
Karthik <karthikalakunta21@gmail.com>
2022-11-12 11:50:48 +0530

    added

--- fetures/template/template.router.js
+++ fetures/template/template.router.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { updateOne, findOneAndUpdate } = require("./template.model");
 const server = express();
 const Template = require("./template.model");
 
@@ -8,33 +7,45 @@ server.get("/", async (req, res) => {
   res.send(Templates);
 });
 
+
 server.get("/:id", async (req, res) => {
   const { id } = req.params;
-
+  
   try {
-    const Templates = await Template.findById(id);
-    res.send(Templates);
-  } catch (e) {
-    res.send(e.message);
+    const Templates = await Template.findById( id );
+    res.send(Templates)
+  } catch(e) {
+    res.send(e.message)
   }
+  
 });
 
-server.patch("/:id", async (req, res) => {
-  let id = req.params.id;
-  try {
-    const template = await Template.findByIdAndUpdate(
-      id,
-      {
-        ...req.body,
-      },
-      {
-        new: true,
-      }
-    );
-    res.send(template);
-  } catch (e) {
-    res.status(404).send("Cant Update the Data it");
-  }
-});
+server.patch("/:id" , async (req , res) => {
+    let  id  = req.params.id;
+
+  
+
+    try {
+
+        const template = await Template.findByIdAndUpdate(id,
+
+            {
+                ...req.body
+            },
+            {
+                new : true
+            }
+
+        );
+        res.send(template)
+        
+    }
+    catch(e) {
+        res.status(404).send('Cant Update the Data it')
+    }
+
+
+
+})
 
 module.exports = server;
