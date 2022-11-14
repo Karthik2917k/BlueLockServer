061eda8578acda3bc7a14b8599d6d7bd680574f9
Karthik <karthikalakunta21@gmail.com>
2022-11-14 11:33:26 +0530

    added

---
+++ fetures/Admin/admin.router.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const server = express();
+const Admin = require("./admin.model.js");
+
+server.get("/", async (req, res) => {
+  try{
+    let user = await User.find();
+    res.send(user);
+  } catch(e) {
+    res.status(401).send(e.message);
+  }
+});
+
+server.post("/signup", async (req, res) => {
+  let { name, email, password } = req.body;
+  try {
+    let existinguser = await Admin.findOne({ email });
+    if (existinguser) {
+      res.status(404).send('we can"t able to create email alreay in use');
+    } else {
+      let user = await Admin.create({
+        name,
+        email,
+        password,
+      });
+
+      res.send({ user });
+    }
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+server.post("/signin", async (req, res) => {
+  let { email, password } = req.body;
+  try {
+    let user = await Admin.findOne({ email });
+    if (user && user.email === email) {
+      if (user.password === password) {
+        res.send({
+          token: `${email}:${password}`,
+          user,
+        });
+      } else {
+        res.status(401).send("Wrong Password");
+      }
+    } else {
+      res.status(401).send("Email not found");
+    }
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+server.get("/singleuser/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Admin.findById(id);
+    res.send(user);
+  } catch (e) {
+    res.status(404).send("Id not found");
+  }
+});
+
+server.patch("/", async (req, res) => {
+  try{
+    const {_id} = req.body
+    const updates = req.body;
+
+    const result = await Admin.findByIdAndUpdate(_id,updates,{new:true});
+    res.send(result)
+  } catch (e) {
+    res.status(404).send(e.message);
+  }
+});
+
+server.delete("/", async (req, res) => {
+  let { _id } = req.body;
+
+  const user = await Admin.findById(_id);
+  if (user) {
+    let Delete = await Admin.deleteOne({ _id });
+    res.status(200).send(`user ${user.name} is deleted successfully`);
+  } else {
+    res.status(401).send("Id Not found");
+  }
+});
+
+module.exports = server;
